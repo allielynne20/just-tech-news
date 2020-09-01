32e368f9057d43bffa1dc5d174b7c8b64042688e
allielynne20 <allielynne20@gmail.com>
2020-09-01 11:27:04 -0400

    pushed files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./user');
 const Post = require('./post');
 const Vote = require('./vote');
+const Comment = require('./comment');
 
 // create associations
 
@@ -42,4 +43,20 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
