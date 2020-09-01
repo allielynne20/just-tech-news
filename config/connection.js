8cfdebd9016a936ba0d1ddaec866427067ef1ab7
allielynne20 <allielynne20@gmail.com>
2020-09-01 16:29:23 -0400

    created MVC files/ folders and started to create the home page stuff

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
+
+const sequelize = new Sequelize('just_tech_news_db', 'root', '13308994tennis!', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
