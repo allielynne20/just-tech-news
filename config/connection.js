a5d159522d36e214b48d49428111c71b95480993
allielynne20 <allielynne20@gmail.com>
2020-09-02 11:50:26 -0400

    added solution code to compare with module

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,34 @@
 // import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // create connection to our db
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// });
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: "localhost",
+//       dialect: "mysql",
+//       port: 3306,
+//     });
 
-const sequelize = new Sequelize('just_tech_news_db', 'root', '13308994tennis!', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize('just_tech_news_db', 'root', '13308994tennis!', {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+// const sequelize = new Sequelize(
+//   "just_tech_news_db",
+//   "root",
+//   "13308994tennis!",
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
 module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const User = require('./user');
-const Post = require('./post');
-const Vote = require('./vote');
-const Comment = require('./comment');
+const User = require("./user");
+const Post = require("./post");
+const Vote = require("./vote");
+const Comment = require("./comment");
 
 // create associations
 
@@ -16,47 +16,56 @@ const Comment = require('./comment');
 //
 
 User.belongsToMany(Post, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'user_id'
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
 });
 
 Post.belongsToMany(User, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'post_id'
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
 });
 
 Vote.belongsTo(User, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Vote.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-
-User.hasMany(Vote, {
-    foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-    foreignKey: 'post_id'
+  foreignKey: "post_id",
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
+  foreignKey: "post_id",
+});
+
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Vote, {
+  foreignKey: "post_id",
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+  foreignKey: "post_id",
 });
 
 module.exports = { User, Post, Vote, Comment };
